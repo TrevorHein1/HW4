0790d579a498fe29ee2f4cdfe703e99a6ee51177
Trevor Hein <trevor-hein@uiowa.edu>
2014-02-20 11:50:47 -0600

    Initial Commit

---
+++ Default.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    
+        <div>
+    <h1> Salary Calculator </h1>
+            <p> Hours:
+                <asp:TextBox ID="HoursTB" runat="server"></asp:TextBox>
+            </p>
+            <p> Wage:
+                <asp:TextBox ID="WageTB" runat="server"></asp:TextBox>
+            </p>
+            <p> Pre Tax:
+                <asp:TextBox ID="PreTaxTB" runat="server"></asp:TextBox>
+            </p>
+            <p> Post Tax Deductions:
+                <asp:TextBox ID="PostTaxTB" runat="server"></asp:TextBox>
+            </p>
+            <p> Net Pay: <asp:Label ID="NetPayLB" runat="server"></asp:Label>
+            </p>
+            <p> 
+                <asp:Button ID="SubmitButton" runat="server" Text="Submit" Width="80px" />
+&nbsp;
+                <asp:Button ID="ClearButton" runat="server" Text="Clear" Width="80px" />
+            </p>
+    
+        
+        
+        
+        </div>
+    
+    
+    
+    
+    </form>
+</body>
+</html>
---
+++ Default.aspx.vb
@@ -0,0 +1,55 @@
+﻿
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub SubmitButton_Click(sender As Object, e As EventArgs) Handles SubmitButton.Click
+
+        'Declare Variables
+        Dim Hours As Decimal
+        Dim Wage As Decimal
+        Dim PreTax As Decimal
+        Dim GrossPay As Decimal
+        Dim Taxable As Decimal
+        Dim Tax As Decimal
+        Dim Pay As Decimal
+        Dim PostTax As Decimal
+
+        'Assign Variables
+        Hours = Val(HoursTB.Text)
+        Wage = Val(WageTB.Text)
+        PreTax = Val(PreTaxTB.Text)
+        PostTax = Val(PostTaxTB.Text)
+
+        'Calculate Gross Pay
+        GrossPay = Hours * Wage
+
+        'Calcualte Taxable Pay
+        Taxable = GrossPay - PreTax
+
+        'Calculate Tax Percentage
+        If GrossPay < 500 Then
+            Tax = Taxable * 0.18
+        Else
+            Tax = Taxable * 0.22
+        End If
+
+        'Calculate Pay
+        Pay = Taxable - Tax
+
+        'Calculate Net Pay
+        NetPayLB.Text = Pay - Val(PostTaxTB.Text)
+
+    End Sub
+
+    Protected Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+
+        'Clear All User Input and Output
+        HoursTB.Text = String.Empty
+        WageTB.Text = String.Empty
+        PreTaxTB.Text = String.Empty
+        PostTaxTB.Text = String.Empty
+        NetPayLB.Text = String.Empty
+
+
+    End Sub
+End Class
